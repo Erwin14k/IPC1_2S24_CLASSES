b976c41da1c3e819a0225f71ae5af033da84a224
Erwin14k <contactwitherwin@gmail.com>
2024-12-18 13:56:33 -0600

    [ADD] Class 10 source added, frontend init

---
+++ Class10/sport-store/src/App.jsx
@@ -0,0 +1,15 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+const App=()=>{
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>}></Route>
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
